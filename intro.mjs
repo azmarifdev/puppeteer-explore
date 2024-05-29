64e0e1b36037cfc4052a8fde659f2f6cd8e520d3
azmarifdev <azmarif.dev@gmail.com>
2024-05-30 02:38:35 +0600

    screenshot

--- intro.mjs
+++ intro.mjs
@@ -7,10 +7,19 @@ const browser = await puppeteer.launch({
     userDataDir: 'temporary',
 });
 const page = await browser.newPage();
-await page.goto('https://www.example.com/', {
+await page.goto('https://www.duckduckgo.com/', {
     waitUntil: 'networkidle2',
-    timeout: 60000,
 });
+await page.waitForSelector('#searchbox_input');
+await page.type('#searchbox_input', 'azmarifdev');
+await page.keyboard.press('Enter');
+await page.waitForSelector('[data-testid="result-title-a"]');
+await page.screenshot({ path: 'duckduckgo.png' });
+
 console.log(await page.title());
-await page.screenshot({ path: 'example.com.png' });
+// await page.screenshot({ path: 'example.com.png' });
 await browser.close();
+
+// id = 'searchbox_input';
+// class="searchbox_searchButton__F5Bwq iconButton_button__6x_9C"
+// data-testid="result-title-a"
