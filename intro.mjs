290d8269a574b027f81817907bae4c10a307f925
azmarifdev <azmarif.dev@gmail.com>
2024-05-30 02:05:59 +0600

    fix executablePath

--- intro.mjs
+++ intro.mjs
@@ -1,1 +1,16 @@
-import puppeteer from "puppeteer-core";
+import puppeteer from 'puppeteer-core';
+const browser = await puppeteer.launch({
+    executablePath: '/usr/bin/google-chrome',
+    headless: false,
+    defaultViewport: { width: 1920, height: 1000 },
+    slowMo: 250,
+    userDataDir: 'temporary',
+});
+const page = await browser.newPage();
+await page.goto('https://www.example.com/', {
+    waitUntil: 'networkidle2',
+    timeout: 60000,
+});
+console.log(await page.title());
+await page.screenshot({ path: 'example.com.png' });
+await browser.close();
