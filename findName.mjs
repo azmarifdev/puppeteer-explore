eb2dd444786abeb02b780536bf6656e278975b6a
azmarifdev <azmarif.dev@gmail.com>
2024-05-30 06:37:14 +0600

    added a Promise function

--- findName.mjs
+++ findName.mjs
@@ -1,31 +1,43 @@
 import puppeteer from 'puppeteer-core';
-const browser = await puppeteer.launch({
-    // executablePath
-    executablePath: '/usr/bin/google-chrome',
-});
 
-// assign search engine
-const page = await browser.newPage();
-await page.goto('https://www.duckduckgo.com/', {
-    waitUntil: 'networkidle2',
-});
+(async () => {
+    try {
+        const browser = await puppeteer.launch({
+            // executablePath
+            executablePath: '/usr/bin/google-chrome',
+            headless: false,
+        });
 
-// type search query
-const searchField = await page.waitForSelector('#searchbox_input');
-await searchField.type('azmarifdev portfolio');
-await page.keyboard.press('Enter');
+        // search engine
+        const page = await browser.newPage();
+        await page.goto('https://www.duckduckgo.com/', {
+            waitUntil: 'networkidle2',
+        });
 
-// click first firstWebsite
-const firstWebsite = await page.waitForSelector('[data-testid="result-title-a"]');
-await firstWebsite.click();
-await page.waitForSelector('.elementor-heading-title');
+        // search field type
+        const searchField = await page.waitForSelector('#searchbox_input');
+        await searchField.type('azmarifdev portfolio');
+        await page.keyboard.press('Enter');
 
-// evaluate
-const name = await page.evaluate(() => {
-    const myNameElement = document.querySelector('#nameid');
-    const nameText = myNameElement.querySelector('.elementor-widget-container .elementor-heading-title').innerHTML;
-    return nameText;
-});
+        // first website click
+        await page.waitForSelector('[data-testid="result-title-a"]');
+        const firstWebsite = await page.$('[data-testid="result-title-a"]');
+        await firstWebsite.click();
 
-console.log(name);
-await browser.close();
+        // wait for page
+        await page.waitForNavigation({ waitUntil: 'networkidle2' });
+        await page.waitForSelector('.elementor-heading-title');
+
+        //  evaluate
+        const name = await page.evaluate(() => {
+            const myNameElement = document.querySelector('#nameid .elementor-widget-container .elementor-heading-title');
+            return myNameElement ? myNameElement.innerHTML : 'Name not found';
+        });
+
+        console.log(name);
+
+        await browser.close();
+    } catch (error) {
+        console.error('An error occurred:', error);
+    }
+})();
