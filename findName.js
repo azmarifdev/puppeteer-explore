20957939d3ba9a6ce7c097f8b07bcb84e2ae88c2
azmarifdev <azmarif.dev@gmail.com>
2024-05-30 18:28:30 +0600

    viewport added in script

--- findName.js
+++ findName.js
@@ -6,6 +6,7 @@ import puppeteer from 'puppeteer';
         const browser = await puppeteer.launch({
             // executablePath
             // executablePath: '/usr/bin/google-chrome',
+            defaultViewport: {width: 1920, height: 1080},
             headless: false,
         });
 
