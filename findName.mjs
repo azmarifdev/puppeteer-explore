ac7380724e91f2d66794e31b4321adee874345ed
azmarifdev <azmarif.dev@gmail.com>
2024-05-30 06:12:37 +0600

    find my name working now

--- findName.mjs
+++ findName.mjs
@@ -1,49 +1,25 @@
-/* import puppeteer from 'puppeteer-core';
-(async () => {
-    const browser = await puppeteer.launch({
-        executablePath: '/usr/bin/google-chrome',
-    });
-    const page = await browser.newPage();
-    await page.goto('https://www.azmarif.dev/', {
-        waitUntil: 'networkidle2',
-    });
-
-    const name = await page.evaluate(() => {
-        const myNameElement = document.querySelector('#nameid');
-        const nameText = myNameElement.querySelector('.elementor-widget-container .elementor-heading-title').innerHTML;
-        return nameText;
-    });
-    console.log(name);
-    await browser.close();
-})(); */
-
 import puppeteer from 'puppeteer-core';
 const browser = await puppeteer.launch({
     executablePath: '/usr/bin/google-chrome',
-    headless: false,
-    defaultViewport: { width: 1920, height: 1000 },
-    slowMo: 100,
-    userDataDir: 'temporary',
 });
 const page = await browser.newPage();
 await page.goto('https://www.duckduckgo.com/', {
     waitUntil: 'networkidle2',
 });
 const searchField = await page.waitForSelector('#searchbox_input');
-await searchField.type('azmarifdev');
+await searchField.type('azmarifdev portfolio');
 await page.keyboard.press('Enter');
-await page.waitForSelector('[data-testid="result-title-a"]');
+const firstWebsite = await page.waitForSelector('[data-testid="result-title-a"]');
 
+await firstWebsite.click();
 
+await page.waitForSelector('.elementor-heading-title');
 
+const name = await page.evaluate(() => {
+    const myNameElement = document.querySelector('#nameid');
+    const nameText = myNameElement.querySelector('.elementor-widget-container .elementor-heading-title').innerHTML;
+    return nameText;
+});
+console.log(name);
 
-
-
-
-// await browser.close();
-// id = 'searchbox_input';
-// class="searchbox_searchButton__F5Bwq iconButton_button__6x_9C"
-// data-testid="result-title-a"
-// console.log(34);
-
-console.log(await page.title());
+await browser.close();
